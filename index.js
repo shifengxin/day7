145bf2a0ea38fce148959521f013ec43aa551a10
shifengxin <2774514376@qq.com>
2019-01-03 08:55:34 +0800

    day7

---
+++ index.js
@@ -0,0 +1,13 @@
+function requ() {
+    //该方法可导入模块
+    //请写出内部逻辑
+    var val = def(2, 3);
+    console.log(val);
+}
+requ();
+
+function def(a, b) {
+    //该方法可定义模块
+    //请写出内部逻辑
+    return a + b;
+}
